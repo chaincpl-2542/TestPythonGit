7389e00d2b4a5f5ffaa5e64e6ad9d8425b9299d5
ChainCPl2542 <chaincpl2542@gmail.com>
2024-09-08 14:52:16 +0700

    first commit

---
+++ lab1.py
@@ -0,0 +1,24 @@
+pi = 3.14
+allResults = []
+
+print('Enter your radius:')
+radius = input()
+
+def circle_area_cal(radius):
+    area = pow(radius,2) * pi
+    allResults.append(area)
+    return area
+    
+for i in range(1,6):
+    #circle_area_cal(i)
+    print(f"the area of {i} is {circle_area_cal(i)}")
+
+
+allResults.append(10)
+allResults.pop(0) 
+print(sum(allResults))
+
+grades = {'Chain' : 'A',
+          'Jab' : 'B'}
+        
+print(grades['Chain'])
